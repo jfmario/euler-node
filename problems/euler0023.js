5c93e49b4415331dd7d178e4a2d10454f8cf6d0f
BuildTools <unconfigured@null.spigotmc.org>
2017-01-13 23:54:10 -0500

    problem 23

--- problems/euler0023.js
+++ problems/euler0023.js
@@ -3,6 +3,7 @@
  * @module problems/euler0023.js
  */
 
+// Borrow function from problem 21
 var sumOfProperDivisors = require ( './euler0021' ).d;
 
 var MAX = 28123;
@@ -11,40 +12,34 @@ function process ( n )
 {
 
     var answer = 0;
-    var abundantNumbers = [];  
-    var sumsOfAbundantNumbers = [];
+    var abundantNumbers = []; 
+    // track sums of abundant numbers
+    var sumsOfAbundantNumbers = {};
+    // sum all numbers 1 to MAX
+    for ( var i = 1; i < n; ++i ) answer += i;
+    // find all abundant numbers
     for ( var i = 12; i <= n; ++i )
     {
         if ( sumOfProperDivisors ( i ) > i )
             abundantNumbers.push ( i );
     }
-    console.log ( "abundantNumbers" );
+    // remove all sums of abundant numbers from the total
     for ( var i = 0; i < abundantNumbers.length; ++i )
     {
         for ( var j = i; j < abundantNumbers.length; ++j )
         {
-            sumsOfAbundantNumbers.push ( abundantNumbers [i] +
-                abundantNumbers [j] );
+            // find a sum of abundant numbers
+            var abundantSum = abundantNumbers [i] +
+                abundantNumbers [j];
+            /* if it hasn't been seen and is less than MAX,
+             * then remove it from total */
+            if ( !sumsOfAbundantNumbers [abundantSum] && abundantSum < n )
+            {
+                // remember this number
+                sumsOfAbundantNumbers [abundantSum] = true;
+                answer -= abundantSum;
+            }
         }
-    }
-
-    sumsOfAbundantNumbers.sort ();
-
-    var index = 0;
-    var arrLength = sumsOfAbundantNumbers.length;
-
-    for ( var i = 0; i <= n; ++i )
-    {
-        if ( i < sumsOfAbundantNumbers [index] )
-            answer += i;
-        else if ( i == sumsOfAbundantNumbers [index] )
-            index++;
-        else
-        {
-            index++;
-            i -= 1; // go back one and check again
-        }
-        if ( index >= arrLength ) break;
     }
 
     return answer;
